df02ff550b921fc9e9c021540972f9bca9abb7b2
sunzheng <465795297@qq.com>
2020-01-05 11:30:45 +0800

    图片上传文件名随机

--- src/main/java/com/bb/bb/controller/common/UploadController.java
+++ src/main/java/com/bb/bb/controller/common/UploadController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @RestController
 public class UploadController {
@@ -19,6 +24,10 @@ public class UploadController {
             return new Result(999,"上传文件不能为空");
         }
 
+        String uploadFilename = file.getOriginalFilename();
+
+        String filename = this.getFileName() + uploadFilename.substring(uploadFilename.lastIndexOf('.'));
+
         File f = new File(this.rootPath);
 
         if(!f.exists()){
@@ -26,11 +35,28 @@ public class UploadController {
                 return new Result(999,f.getPath() + "目录创建失败");
             }
         }
-
-        File dir = new File(this.rootPath + file.getOriginalFilename());
+        String filepath = this.rootPath + filename;
+        File dir = new File(filepath);
 
         file.transferTo(dir);
 
-        return new Result<>('0',"success");
+        Map<String,String> resultData = new HashMap<String,String>();
+        resultData.put("path",filepath);
+
+        return new Result<>(0,"success",resultData);
+    }
+
+    protected String getFileName(){
+
+        Date d = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddkkmmss");
+
+        String filename = sdf.format(d);
+
+        Random r = new Random();
+
+        filename += r.nextInt(100)+ "";
+
+        return filename;
     }
 }
